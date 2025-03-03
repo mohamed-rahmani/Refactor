7dd02df28a0b1905289e27f8e82217a6c6740cc7
mohamed-rahmani <bouchkhaf@gmail.com>
2025-03-03 09:55:28 +0100

    etape 7 : refactor de la méthode getCharge pour déléguer la responsabilité à la classe Movie

--- src/main/java/fr/r6a06/Movie.java
+++ src/main/java/fr/r6a06/Movie.java
@@ -21,4 +21,25 @@ public class Movie {
     public String getTitle (){
         return _title;
     };
+
+    public double getMoviePrice(int daysRented){
+        double result = 0.0;
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
