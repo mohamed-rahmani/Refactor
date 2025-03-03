4771be842c39dee864fcd3c5a15be31645a51c25
bl <bouchkhaf@gmail.com>
2025-03-03 17:45:39 +0100

    step10 : build pattern

--- src/main/java/fr/r6a06/Price.java
+++ src/main/java/fr/r6a06/Price.java
@@ -5,4 +5,18 @@ public abstract class Price {
     public abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
+
+    public static class PriceBuilder {
+        private int priceCode;
+
+        public PriceBuilder withPriceCode(int priceCode) {
+            this.priceCode = priceCode;
+            return this;
+        }
+        public Price build() {
+            if (priceCode == 0) { return new RegularPrice(); }
+            if (priceCode == 1) { return new NewReleasePrice(); }
+            return new ChildrensPrice();
+        }
+    }
 }
